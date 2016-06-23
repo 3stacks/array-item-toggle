11529765007e5ccfb135ffe0970a036efcc65612
Luke Boyle <luke.boyle.au@gmail.com>
2016-06-23 19:53:15 +1000

    feat: add the toggleArrayItem function

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 'use strict';
 
-Object.defineProperty(exports, '__esModule', { value: true });
+var toConsumableArray = function (arr) {
+  if (Array.isArray(arr)) {
+    for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i];
 
-function helloWorld(message) {
-    console.log(message);
+    return arr2;
+  } else {
+    return Array.from(arr);
+  }
+};
+
+/**
+ * @param {Array} array
+ * @param {*} item
+ * @returns {Array} - New array with the item added or removed.
+ */
+function toggleArrayItem(array, item) {
+    var isItemInArray = array.includes(item);
+    if (isItemInArray) {
+        return array.filter(function (arrayItem) {
+            return arrayItem !== item;
+        });
+    } else {
+        return [].concat(toConsumableArray(array), [item]);
+    }
 }
 
-exports.helloWorld = helloWorld;
+module.exports = toggleArrayItem;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-export function helloWorld(message) {
-    console.log(message)
+/**
+ * @param {Array} array
+ * @param {*} item
+ * @returns {Array} - New array with the item added or removed.
+ */
+export default function toggleArrayItem(array, item) {
+    const isItemInArray = array.includes(item);
+    if (isItemInArray) {
+        return array.filter(arrayItem => arrayItem !== item);
+    } else {
+        return [
+            ...array,
+            item
+        ]
+    }
 }
