badb0b8aa42bae763de2cb8cb9b27a97e4f52a22
Luke Boyle <luke.boyle.au@gmail.com>
2016-06-24 08:38:29 +1000

    chore: export function default

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
  * @param {*} item
  * @returns {Array} - New array with the item added or removed.
  */
-export default function toggleArrayItem(array, item) {
+export default function(array, item) {
     const isItemInArray = array.includes(item);
     if (isItemInArray) {
         return array.filter(arrayItem => arrayItem !== item);
